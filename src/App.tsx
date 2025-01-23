3fffe500785087657689d756823947ab8ca9e6e3
terry0505 <hana1155@hanmail.net>
2025-01-23 13:16:23 +0900

    실습중

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import styles from '@/assets/styles/common.module.scss';
 import Header from './components/common/Header';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div className={styles.wrapper}>
-        <Header/>
-        <div className={styles.container}>
-            <Outlet/>
+    <AuthContextProvider>
+        <div className={styles.wrapper}>
+            <Header/>
+            <div className={styles.container}>
+                <Outlet/>
+            </div>
         </div>
-    </div>
+    </AuthContextProvider>
   );
 }
 
